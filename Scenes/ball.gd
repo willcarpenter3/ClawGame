c93ad6acaaefc7df8b18437586fd6ae9f243624b
Will Carpenter <willcarpenter3673@gmail.com>
2026-04-12 11:52:38 -0400

    Added a ball pit and ball scene to test physics bodies

---
+++ Scenes/ball.gd
@@ -0,0 +1,14 @@
+extends RigidBody3D
+
+@onready var mesh := $MeshInstance3D
+
+func initialize(pos: Vector3, newScale: Vector3):
+	position = pos
+	
+	var material = StandardMaterial3D.new()
+	material.albedo_color = Color(randf(), randf(), randf(), 1)
+	material.roughness = 0.25
+	material.metallic = 0.75
+	mesh.material_override = material
+	
+	scale = newScale
---
+++ Scenes/spawner.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+@export var thingToSpawn : PackedScene
+@export var numToSpawn := 3
+@export var spawnRadius : float
+@export var scaleRandomness := 0.2
+
+@onready var spawn_loc = $SpawnPath/SpawnPathLoc
+
+
+func _on_timer_timeout() -> void:
+	print("Timer going off!")
+	
+	
+	var spawnedThing = thingToSpawn.instantiate()
+	var spawnX = randf_range(-spawnRadius, spawnRadius) + position.x
+	var spawnZ = randf_range(-spawnRadius, spawnRadius) + position.z
+	
+	add_child(spawnedThing)
+	spawnedThing.initialize(Vector3(spawnX, position.y, spawnZ), Vector3.ONE * (1 + randf_range(-scaleRandomness, scaleRandomness)))
+	
